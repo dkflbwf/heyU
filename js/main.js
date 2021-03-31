ad6fab96067942e8ef145628ebaea52f8b00a657
Vladislav <72874576+dkflbwf@users.noreply.github.com>
2021-03-31 15:53:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function slowScroll(id) {
+    $("html, body").animate({
+        scrollTop: $(id).offset().top - 50
+    }, 500);
+    return false;
+}
+
+$('.header-top .menu').on('click', function() {
+    if($('header .mobile-menu').is(':visible'))
+        $(this).html('<i class="fas fa-door-open"></i>');
+    else
+        $(this).html('<i class="fas fa-door-closed"></i>');
+    $('header .mobile-menu').slideToggle();
+});
+
+$('.video-play, #modal-video .close-button').on('click', function() {
+  $('#modal-video').toggle();
+  $('body').toggleClass('overflow-hidden');
+  resizeVideo();
+});
+
+$('#subscribe').on('click', function() {
+    let email = $('#email').val();
+    email = email.trim();
+    if (email.split('@').length != 2 || email.split('.').length != 2) {
+        $('#sub_form label').text('Вы ввели неверный email');
+        $('#sub_form label').fadeIn();
+    }
+
+    setTimeout(function() {
+        $('#sub_form label').fadeOut();
+    }, 700);
+});
+
+$(window).on('resize', function() {
+    resizeVideo();
+}).resize();
+
+function resizeVideo() {
+    $('iframe').each(function() {
+        let width = $(this).width();
+        $(this).css('height', width / 1.77 + 'px');
+    });
+}
